0d84796cb9bb95305da2d22bbe493ccc3dd7d8ec
travelerslibrary <no.diet.franchise@gmail.com>
2026-06-13 03:27:29 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "travelers-library-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+
+  "/assets/apple-touch-icon.png",
+  "/assets/favicon.png",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png",
+  "/assets/og-card.PNG"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
